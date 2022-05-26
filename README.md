ed224558d39b7f906f622f2c8c048bb43e412d33
hxf0223 <hxf0223@hotmail.com>
2022-05-26 11:32:09 +0800

    add glog

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 ## README
 
 ### 开发环境
-- VS2022扩展：Clang Power Tools
-- 使用gtest源码 [v1.11.0](https://github.com/google/googletest/releases/tag/release-1.11.0)。
+- VS2022扩展：Clang Power Tools ；
+- 依赖于boost，需要定义系统环境变量 BOOST_ROOT；
+- 使用gtest源码 [v1.11.0](https://github.com/google/googletest/releases/tag/release-1.11.0)；
+- 使用glog源码 [v0.6.0](https://github.com/google/glog/releases/tag/v0.6.0)。
 
 gtest更新的版本需要 Abseil编译，可能会比较麻烦。
+gtest源码目录相对于官方gtest仓库修改：
+- googletest/CMakeLists.txt：set(GOOGLETEST_VERSION 1.11.0)，否则找不到GOOGLETEST_VERSION会报错；
+- googletest/cmake/internal_utils.cmake：函数cxx_library_with_type中更改RUNTIME_OUTPUT_DIRECTORY等变量的路径为CMAKE_ARCHIVE_OUTPUT_DIRECTORY，使输出路径与父CMakeLists.txt保持一致。
+- googletest/CMakeLists.txt：添加 set(gtest ${gtest} PARENT_SCOPE)，让test目录能够依赖于gtest之后编译；
+- googletest/CMakeLists.txt：gtest，gtest_main 静态编译修改为 cxx_shared_library 动态编译。
+
+glog源码相对于官方glog的修改：
+- glog/CMakeLists.txt： set(BUILD_TESTING OFF)，禁用编译glog的测试用例；
+- glog/CMakeLists.txt：set(glog ${glog} PARENT_SCOPE)；
+- 添加 glog/include/glog目录，将install得到的头文件拷贝到此目录下。
 
 ### 参考
 - [CMake设置MSVC工程MT/MTd/MD/MDd](https://blog.csdn.net/Copperxcx/article/details/123084367)
--- test/main.cc
+++ test/main.cc
@@ -16,21 +16,19 @@
 #include <boost/make_unique.hpp>
 #include <boost/thread.hpp>
 
-//#include "gtest/gtest.h"
+#include "glog/logging.h"
+#include "gtest/gtest.h"
 
 #define BOOST_DATE_TIME_SOURCE
 #define BOOST_THREAD_NO_LIB
-
-static void receive_func(const uint8_t *);
 
 constexpr DWORD devtype = 4;
 constexpr DWORD devid = 0;
 constexpr DWORD channel = 0;
 
-int main() {
+TEST(CAN, F001_P0) {
   auto result = VCI_OpenDevice(devtype, 0, 0);
-  std::cout << "VCI_OpenDevice call result: " << result << std::endl;
-  std::cout << "vciobjdatasize: " << sizeof(VCI_CAN_OBJ) << std::endl;
+  CHECK(0 == result) << "VCI_OpenDevice fail: " << result;
 
   VCI_INIT_CONFIG cfg{};
   cfg.Timing0 = 0x00;
@@ -43,9 +41,6 @@ int main() {
 
   result = VCI_InitCAN(devtype, devid, channel, &cfg);
   result = VCI_StartCAN(devtype, devid, channel);
-
-  uint8_t brun = true;
-  // auto up_thread = boost::make_unique<boost::thread>(&receive_func, &brun);
 
   VCI_CAN_OBJ can_obj;
   uint8_t send_data[] = {0x03, 0x22, 0x28, 0x00, 0xAA, 0xAA, 0xAA, 0xAA};
@@ -64,45 +59,24 @@ int main() {
   const auto start = boost::chrono::high_resolution_clock::now();
   boost::chrono::microseconds us;
 
-  // 获取高精度系统时间
-  // boost::posix_time::microsec_clock::local_time()
-
   for (int i = 0; i < 4000; i++, can_obj.Data[2]++) {
-    // OutputDebugString("time point 1\n");
     result = VCI_Transmit(devtype, devid, channel, &can_obj, 1);
-    // OutputDebugString("time point 2\n");
-
-#if 1
     auto len = VCI_Receive(devtype, devid, channel, can_recv_buff, 1, 100);
-    // OutputDebugString("time point 3\n");
-    // getchar();
-
     if (len > 0) {
       std::string str = can::utils::bin2hex_dump(can_recv_buff[0].Data, 8);
-      std::cout << "VCI_Receive: " << std::setfill('0') << std::hex << std::setw(8) << can_recv_buff[0].ID
-                << ", data: " << str << std::endl;
+      LOG(INFO) << "VCI_Receive: " << std::setfill('0') << std::hex << std::setw(8) << can_recv_buff[0].ID
+                << ", data: " << str;
     } else {
-      std::cout << "VCI_Receive fail" << std::endl;
+      LOG(WARNING) << "VCI_Receive fail.";
     }
-#endif
-    // Sleep(100);
   }
 
-  getchar();
-  return 0;
+  result = VCI_CloseDevice(devtype, 0);
+  CHECK(0 == result) << "VCI_CloseDevice fail: " << result;
 }
 
-void receive_func(const uint8_t *pbRunning) {
-#define CAN_OBJ_BUFF_SIZE 1
-  VCI_CAN_OBJ can_recv_buff[CAN_OBJ_BUFF_SIZE];
-
-  while (*pbRunning) {
-    memset(can_recv_buff, 0, sizeof(VCI_CAN_OBJ) * CAN_OBJ_BUFF_SIZE);
-    auto len = VCI_Receive(devtype, devid, channel, can_recv_buff, CAN_OBJ_BUFF_SIZE, 10);
-    if (len <= 0) continue;
-
-    std::string str = can::utils::bin2hex_dump(can_recv_buff[0].Data, 8);
-    std::cout << "VCI_Receive: " << std::setfill('0') << std::hex << std::setw(8) << can_recv_buff[0].ID
-              << ", data: " << str << std::endl;
-  }
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
 }
