42c51d547e06d44aecb443a77585ba058462deb5
hxf0223 <hxf0223@hotmail.com>
2022-06-01 20:43:51 +0800

    update sources structure to fit linux build

---
+++ cmake/platform_include.cmake
@@ -0,0 +1,5 @@
+#ifdef _WIN32
+    include_directories("${PROJECT_SOURCE_DIR}/install_include/win32")
+#elif __linux__
+    
+#endif
--- libControlCAN/inc/lib_control_can_define.hpp
+++ libControlCAN/inc/lib_control_can_define.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "usbcan.h"
-#include <Windows.h>
 #include <iostream>
 #include <memory>
 
