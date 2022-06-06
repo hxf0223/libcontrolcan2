6e0d2a5715111b518231691c2ada7ea562c840aa
hxf0223 <hxf0223@hotmail.com>
2022-06-06 08:29:11 -0700

    build success on ubuntu 2004

---
+++ install_include/linux/glog/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hxf0223/work/libcontrolcan2/glog
+# Build directory: /home/hxf0223/work/libcontrolcan2/build/glog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
