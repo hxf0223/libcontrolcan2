794ea6d883e536ac7e5a64732062d64ceaf12873
hxf0223 <hxf0223@hotmail.com>
2022-06-06 08:33:09 -0700

    remove usless files

--- install_include/linux/glog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hxf0223/work/libcontrolcan2/glog
-# Build directory: /home/hxf0223/work/libcontrolcan2/build/glog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
