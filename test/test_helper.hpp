494f4ccdc5b1edb6761c6422cf54be5c04cd8ccd
hxf0223 <hxf0223@hotmail.com>
2022-06-22 19:03:38 +0800

    fix: test case for CAN DC

--- test/test_helper.hpp
+++ test/test_helper.hpp
@@ -5,11 +5,11 @@
 namespace test {
 namespace helper {
 
-static inline VCI_INIT_CONFIG create_vci_init_cfg(UCHAR timing0, UCHAR timing1, DWORD filter) {
+static inline VCI_INIT_CONFIG create_vci_init_cfg(UCHAR timing0, UCHAR timing1, DWORD mask) {
   VCI_INIT_CONFIG cfg{};
   cfg.Timing0 = timing0;
   cfg.Timing1 = timing1;
-  cfg.Filter = filter;
+  cfg.AccMask = mask;
   return cfg;
 }
 
