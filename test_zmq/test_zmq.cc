574c074514b4da1acca8e26c963487e06919ce9b
hxf0223 <hxf0223@hotmail.com>
2022-08-03 07:32:42 -0700

    finish zmq test case

--- test_zmq/test_zmq.cc
+++ test_zmq/test_zmq.cc
@@ -1,4 +1,5 @@
 #include <atomic>
+#include <csignal>
 #include <cstdio>
 #include <cstring>
 #include <thread>
@@ -18,10 +19,14 @@ std::ostream &operator<<(std::ostream &os, std::optional<T> const &opt) {
 
 } // namespace
 
+static std::atomic_bool run_flag{true};
+static void signal_handler(int signal) { run_flag.store(false); }
+
 TEST(zmq, server) {
   zmq::context_t ctx;
+  signal(SIGINT, signal_handler);
 
-  auto pub_func = [](std::atomic_bool &runFlag, zmq::context_t *ctx) -> void {
+  auto pub_func = [](std::atomic_bool *runFlag, zmq::context_t *ctx) -> void {
     zmq::socket_t publisher(*ctx, zmq::socket_type::pub);
     publisher.bind("inproc://#1");
 
@@ -30,24 +35,33 @@ TEST(zmq, server) {
 
     int32_t msg_id{0};
     char send_buff[256];
-    while (runFlag.load()) {
-      sprintf(send_buff, "publish message %d\n", msg_id++);
-      const auto size = strlen(send_buff) + 1;
+    while (runFlag->load()) {
+      sprintf(send_buff, "publish message %d", msg_id++);
+      const auto size = strlen(send_buff);
       publisher.send(zmq::buffer(std::string_view(send_buff, size)));
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
   };
 
-  auto sub_func = [](std::atomic_bool &runFlag, zmq::context_t *ctx) -> void {
+  auto sub_func = [](std::atomic_bool *runFlag, zmq::context_t *ctx) -> void {
     zmq::socket_t ctx_sub_(*ctx, ZMQ_SUB);
     ctx_sub_.connect("inproc://#1");
     ctx_sub_.set(zmq::sockopt::subscribe, ""); // subscribe all message types
     ctx_sub_.set(zmq::sockopt::rcvtimeo, 10);  // receive timeout in ms
 
-    while (runFlag.load()) {
+    while (runFlag->load()) {
       std::vector<zmq::message_t> recv_msgs;
       zmq::recv_result_t result = zmq::recv_multipart(ctx_sub_, std::back_inserter(recv_msgs));
-      LOG(INFO) << "receive size: " << result;
+      // if (!result.has_value()) continue;
+      for (auto &msg : recv_msgs) {
+        std::string rxstr(static_cast<const char *>(msg.data()), msg.size());
+        std::cout << "receive message: " << rxstr << std::endl;
+      }
     }
   };
+
+  std::thread pub_thd(pub_func, &run_flag, &ctx);
+  std::thread sub_thd(sub_func, &run_flag, &ctx);
+  pub_thd.join();
+  sub_thd.join();
 }
