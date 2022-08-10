14342713d127e2d40dcd3ed58b51af315e085971
hxf0223 <hxf0223@hotmail.com>
2022-08-10 07:55:24 -0700

    update asio udp test

--- test/test_asio_udp.cc
+++ test/test_asio_udp.cc
@@ -50,25 +50,26 @@ TEST(asio, udpServer) {
   service.run();
 }
 
-class listener {
+class udpClient {
   // http://coliru.stacked-crooked.com/a/ae4b87381182c76b
   using ec_t = boost::system::error_code;
 
 public:
-  listener(const boost::asio::ip::udp::endpoint &listen_endpoint)
-    : socket_(io_service_, listen_endpoint), deadline_(io_service_) {
+  udpClient(const boost::asio::ip::udp::endpoint &listenEndpoint)
+    : socket_(io_service_, listenEndpoint), deadline_(io_service_) {
+    socket_.set_option(boost::asio::socket_base::broadcast(true));
+    socket_.set_option(boost::asio::socket_base::reuse_address(true));
     deadline_.expires_at(boost::posix_time::pos_infin);
     check_deadline();
   }
 
-  std::size_t receive(const boost::asio::mutable_buffer &buffer, boost::posix_time::time_duration timeout,
-                      boost::system::error_code &ec) {
+  std::size_t receive(const boost::asio::mutable_buffer &buffer, boost::posix_time::time_duration timeout, ec_t &ec) {
     deadline_.expires_from_now(timeout);
     ec = boost::asio::error::would_block;
     std::size_t length = 0;
     boost::asio::ip::udp::endpoint unused_sender_endpoint;
     socket_.async_receive_from(boost::asio::buffer(buffer), unused_sender_endpoint,
-                               boost::bind(&listener::handle_receive, _1, _2, &ec, &length));
+                               boost::bind(&udpClient::handle_receive, _1, _2, &ec, &length));
 
     // TODO: The following do/while is hinky. Does run_one() need to happen before the comparison?
     do {
@@ -85,7 +86,7 @@ private:
       socket_.cancel();
       deadline_.expires_at(boost::posix_time::pos_infin);
     }
-    deadline_.async_wait(boost::bind(&listener::check_deadline, this));
+    deadline_.async_wait(boost::bind(&udpClient::check_deadline, this));
   }
 
   static void handle_receive(const ec_t &ec, std::size_t length, ec_t *out_ec, std::size_t *out_length) {
@@ -101,7 +102,7 @@ private:
 
 TEST(asio, udpClient) {
   boost::asio::ip::udp::endpoint listen_endpoint(boost::asio::ip::address::from_string("0.0.0.0"), udpPort);
-  listener c(listen_endpoint);
+  udpClient c(listen_endpoint);
 
   for (;;) {
     char data[1024];
