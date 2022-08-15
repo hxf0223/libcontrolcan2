24ffd69cdd7fe66fb1ffb3096e723b51fe49e6e9
hxf0223 <hxf0223@hotmail.com>
2022-08-15 03:38:29 -0700

    format

--- test_asio_udp/udp_server/UDPServer.cpp
+++ test_asio_udp/udp_server/UDPServer.cpp
@@ -1,23 +1,20 @@
 #include "UDPServer.h"
 
 void UDPServer::startReceive() {
-socket_.async_receive_from(
-  boost::asio::buffer(recvBuffer_), remoteEndpoint_,
-  boost::bind(&UDPServer::handleReceive, this,
-              boost::asio::placeholders::error,
-              boost::asio::placeholders::bytes_transferred));
-data_ = std::string(std::begin(recvBuffer_), std::end(recvBuffer_));
-recvBuffer_.fill(0);
-std::cout << data_ << std::endl;
+  socket_.async_receive_from(boost::asio::buffer(recvBuffer_), remoteEndpoint_,
+                             boost::bind(&UDPServer::handleReceive, this, boost::asio::placeholders::error,
+                                         boost::asio::placeholders::bytes_transferred));
+  data_ = std::string(std::begin(recvBuffer_), std::end(recvBuffer_));
+  recvBuffer_.fill(0);
+  std::cout << data_ << std::endl;
 }
 
-void UDPServer::handleReceive(const boost::system::error_code& error,
-                 std::size_t bytes_transferred) {
-if (!error || error == boost::asio::error::message_size) {
+void UDPServer::handleReceive(const boost::system::error_code &error, std::size_t bytes_transferred) {
+  if (!error || error == boost::asio::error::message_size) {
 
-  auto message = std::make_shared<std::string>("Hello, World! (Server)\n");
+    auto message = std::make_shared<std::string>("Hello, World! (Server)\n");
 
-  socket_.send_to(boost::asio::buffer(*message), remoteEndpoint_);
-  startReceive();
-}
+    socket_.send_to(boost::asio::buffer(*message), remoteEndpoint_);
+    startReceive();
+  }
 }
